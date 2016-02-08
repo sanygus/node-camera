d398fca8f918c6bee8486d932389db1df5168c40
Sasha Gusarov <sashagusarov@mail.ru>
2016-02-09 01:33:23 +0300

    changes 2

--- src/statisticWriter.js
+++
@@ -1,9 +0,0 @@
-var options = require('./camOptions');
-var fs = require('fs');
-var path = require('path');
-// var dateformat = require('dateformat');
-
-module.exports = function statisticsWriter(object) {
-  fs.appendFile(path.resolve(options.statisticsFile), JSON.stringify(object));
-};
-/* TODO: add date */
